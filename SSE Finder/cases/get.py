e33d2430704d5ec9335bd79e387564cd044b57ce
comp3297isbad <ihatenetworking999@gmail.com>
2021-04-25 15:28:39 +0800

    Update the model

---
+++ SSE Finder/cases/get.py
@@ -0,0 +1,25 @@
+# import sys, urllib, pprint, requests, json
+
+# address = sys.argv[1].replace(" ", "%20")
+# version = "v1.0.0"
+
+# # remark the version in the geodata for rquesting the data may change into the newer version please update
+# response = requests.get("https://geodata.gov.hk/gs/api/" + version + "/locationSearch?q=" + address)
+
+# print("\nResponse status: %d\n"%response.status_code)
+# pprint.pprint(response.json())
+
+import requests. json
+
+VERSION = "v1.0.0"
+
+def retrive_Data(location):
+    address = address.replace(" ", "%20")
+    response = requests.get("https://geodata.gov.hk/gs/api/" + version + "/locationSearch?q=" + address)
+    if(response.status_code == 200):
+        return(response['addressEN'], response['x'], response['y'])
+    else:
+        return tuple()
+
+
+
--- SSE Finder/cases/models.py
+++ SSE Finder/cases/models.py
@@ -1,29 +1,37 @@
 from django.db import models
+from get import retrive_Data
 
 # Create your models here.
-class Case(models.Model):
-	caseNumber = models.IntegerField()
-	personName = models.CharField(max_length=200)
-	idNumber = models.CharField(max_length=200)
-	dateOfBirth = models.DateField()
-	dateOfOnset = models.DateField()
-	dateCaseConfirmed = models.DateField()
-    
+class Infector(models.Model):
+    case_number = models.IntegerField(unique=True)
+    person_name = models.CharField(max_length=100)
+    document_number = models.CharField(max_length=100, unique=True)
+    date_of_birth = models.DateField()
+    date_of_onset = models.DateField()
+    date_of_confirmation = models.DateField()
+
 	def __str__(self):
-		return self.caseNumber
+		return self.case_number
 
 	def get_absolute_url(self):
-		return reverse('caseInfo', args=[str(self.caseNumber)])
+		return reverse('caseInfo', args=[str(self.case_number)])
 
 class Event(models.Model):
-	Case = models.ForeignKey(Case, on_delete=models.CASCADE)
-	venueName = models.CharField(max_length=200)
-	venueLocation = models.CharField(max_length=200)
-	address = models.CharField(max_length=200)
-	x_coord = models.FloatField()       #auto
-	y_coord = models.FloatField()       #auto
-	dateOfEvent = models.DateField(max_length=200)
-	description = models.CharField(max_length=200)    #(informal)
+    infector = models.ForeignKey(Infector, on_delete=models.CASCADE)
+    venue_name = models.CharField(max_length=100)
+    venue_location = models.CharField(max_length=1000)
+    venue_address = models.CharField(max_length=1000) # generate by function 
+    venue_x_coord = models.IntegerField() # generate by function
+    venue_y_coord = models.IntegerField() # generate by function 
+    date_of_event = date_of_confirmation = models.DateField()
+    description = models.CharField(max_length=3000)
 
-	def __str__(self):
-		return self.venueName
+    def save(self, *args, **kwargs):
+        data = retrive_Data(self.venue_location)
+        if (len(data) == 0):
+            return
+        else:
+            self.venue_address = data[0]
+            self.venue_x_coord = data[1]
+            self.venue_y_coord = data[2]
+        super().save(*args, **kwargs)
