2d2f5610b0bcda7440a5fcdcee94aa149ad1555d
comp3297isbad <ihatenetworking999@gmail.com>
2021-04-26 13:25:43 +0800

    case form post

--- SSE Finder/cases/views.py
+++ SSE Finder/cases/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_detail(request):
+    return render(request, 'cases/event.html')
+
+def case_detail(request):
+    if request.method == 'GET':
+        return render(request, 'cases/case.html')
+    elif request.method == 'POST':
+        case_number = request.POST.get('case_number')
+        person_name = request.POST.get('person_name')
+        document_number = request.POST.get('document_number')
+        date_of_birth = request.POST.get('date_of_birth')
+        date_of_onset = request.POST.get('date_of_onset')
+        date_of_confirmation = request.POST.get('date_of_confirmation')
+        temp = Infector.objects.create(case_number=case_number, person_name=person_name, document_number=document_number, date_of_birth=date_of_birth, date_of_onset=date_of_onset, date_of_confirmation= date_of_confirmation)
+        return render(request, 'cases/event.html')
